03b80b84b0fdba513b37ee9cbadd7b808041ec52
Keith McDonnell <keith@dancingtext.com>
2024-02-06 14:28:43 +0000

    Fix: test config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ Discourse::Application.configure do
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
-  config.cache_classes = !ENV["CI"].present?
+  config.cache_classes = false
 
   # Eager loading loads your entire application. When running a single test locally,
   # this is usually not necessary, and can slow down your test suite. However, it's
